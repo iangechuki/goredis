d0edef3274f32b2d6125e40da4f780171b897e80
Ian Gechuki <iangechuki@gmail.com>
2024-12-07 23:42:59 +0300

    aof implementation to persist data on disk

--- main.go
+++ main.go
@@ -13,13 +13,33 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid commad: ", command)
+			return
+		}
+		handler(args)
+	})
+
+	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
-	//infinite loop to receive commands from clients and respond to them
+
+	// infinite loop to receive commands from clients and respond to them
 	for {
 		resp := NewResp(conn)
 		value, err := resp.Read()
@@ -46,10 +66,12 @@ func main() {
 			writer.Write(Value{typ: "error", str: "unknown command"})
 			continue
 		}
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
 		result := handler(args)
 		writer.Write(result)
-		// writer.Write(Value{typ: "string", str: "OK"})
-		// conn.Write([]byte("+OK\r\n"))
+
 	}
 
 }
